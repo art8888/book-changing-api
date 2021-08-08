823e36fb2dbc4481fa5995fb2624a926c93bfaec
Arturas <info@webai.lt>
2021-08-08 16:55:32 +0300

    Header component

---
+++ front/src/global/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { AppBar, Toolbar, IconButton  } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+
+export default function Header() {
+    const classes = useStyles();
+
+    const displayTitle = () => {
+        return (
+        <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu"></IconButton>
+            <Typography variant="h6" className={classes.title}>Books changing API</Typography>
+        </Toolbar>
+        );
+    }
+
+    return (
+        <AppBar position="static">{displayTitle()}</AppBar>
+    )
+}    
